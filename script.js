c7a23bb5546b435c06def1ab169e72de6c14f87c
alestucci <alessandrotucci@gmail.com>
2022-02-24 15:23:41 +0100

    Algoritmo JS

---
+++ script.js
@@ -0,0 +1,12 @@
+/* MAIL
+- generare array indirizzi email
+- inserire nella pagina campo input e bottone
+- chiedere utente indirizzo email
+- controllare che il valore del campo sia un indirizzo email valido
+- confrontare input con dati array
+- se presente output "vai al login" else "utente non registrato"
+
+/* DADO
+- assegnare a una variabile (utente) un numero casuale da 1 a 6
+- assegnare a una variabile (computer) un numero casuale da 1 a 6
+- se numeri uguali pareggio, se utente > computer vince utente, else vince computer (switch)*/
